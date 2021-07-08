391b85388b86a381d59b10620f4e793d5237ea4d
YeonHo <kiteho8962@naver.com>
2021-07-09 01:19:01 +0900

    uploadcontroller && upload

---
+++ src/main/java/com/soondae/camp/file/controller/BoardImageController.java
@@ -0,0 +1,71 @@
+package com.soondae.camp.file.controller;
+
+import com.soondae.camp.file.dto.BoardImageDTO;
+import lombok.extern.log4j.Log4j2;
+import net.coobird.thumbnailator.Thumbnails;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.MediaType;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.multipart.MultipartFile;
+
+import javax.imageio.ImageIO;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
+import java.nio.file.Files;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.UUID;
+
+@Controller
+@Log4j2
+public class BoardImageController {
+
+    @Value("${spring.servlet.multipart.location}")
+    private String path;
+
+    @GetMapping(value = "/check")
+    public @ResponseBody String testCheck() {
+        return "abc";
+    }
+
+
+    @ResponseBody
+    @PostMapping(value = "/upload", produces = MediaType.APPLICATION_JSON_VALUE)
+    public List<BoardImageDTO> upload(MultipartFile[] files) {
+
+        List<BoardImageDTO> result = new ArrayList<>();
+
+        for (MultipartFile file:files) {
+            String originalFilename = file.getOriginalFilename();
+            String uuid = UUID.randomUUID().toString();
+
+            File outFile = new File(path, uuid+"_"+originalFilename);
+            File thumbFile = new File(path, "s_"+uuid+"_"+originalFilename);
+
+            try {
+                InputStream fin = file.getInputStream();
+                Files.copy(fin, outFile.toPath());
+
+                BufferedImage originalImage = ImageIO.read(outFile);
+                BufferedImage thumbnail = Thumbnails.of(originalImage)
+                        .size(150, 150)
+                        .asBufferedImage();
+
+                ImageIO.write(thumbnail, "PNG", thumbFile);
+                fin.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            result.add(BoardImageDTO.builder()
+                    .fuuid(uuid)
+                    .fname(originalFilename)
+                    .build());
+        } // end loop
+        return result;
+    }
+}
